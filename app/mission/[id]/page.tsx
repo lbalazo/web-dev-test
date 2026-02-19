8322b4fee61855b9698ecb2b108ea386feb03c6c
Laurence Balazo <laurence@goodfrontend.dev>
2026-02-20 00:08:12 +0800

    feat(metadata): Add metadata on home and details page

--- app/mission/[id]/page.tsx
+++ app/mission/[id]/page.tsx
@@ -1,6 +1,20 @@
 import { getLaunch, getRocket } from "@/lib/spacex";
 import { Launch, Rocket } from "@/app/types/launch";
 import MissionDetail from "@/components/mission/MissionDetail";
+import { Metadata } from "next";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { id: string };
+}): Promise<Metadata> {
+  const { id } = await params;
+  const launch: Launch = await getLaunch(id);
+  return {
+    title: `${launch.name} | SpaceX Missions`,
+    description: launch.details ?? `Details for SpaceX mission ${launch.name}`,
+  };
+}
 
 export default async function MissionPage({
   params,
